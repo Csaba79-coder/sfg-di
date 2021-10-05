ce13c16d944743d38e92957f1b1ed48048d84b3d
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-06 00:48:28 +0200

    creating controllers, and tests also testing constuctor and seter

---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController controller;
+
+    // right click generate setup method!
+    @BeforeEach
+    void setUp() {
+        controller = new PropertyInjectedController();
+        controller.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    void getGreeting() {
+        // works without sout as well!
+        System.out.println(controller.getGreeting());
+    }
+}
