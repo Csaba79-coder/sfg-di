cbf312762f95700c950285239105fc2348dcf05b
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-06 23:18:05 +0200

    Revert "Revert "do refactor""
    This reverts commit 0741a5eff863401d5cdd202b225ce8ddf34b7790.

--- src/main/java/guru/springframework/sfgdi/services/CustomBeanPostProcessor.java
+++ src/main/java/guru/springframework/sfgdi/services/CustomBeanPostProcessor.java
@@ -4,15 +4,19 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
+// this one give additional information about the beans!
+// e.g: ## - Before Init - Called by Bean Post Processor
+// and also this one: ## - After init called by Bean Post Processor
+
 @Component
 public class CustomBeanPostProcessor implements BeanPostProcessor {
+
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 
         if(bean instanceof LifeCycleDemoBean){
             ((LifeCycleDemoBean) bean).beforeInit();
         }
-
         return bean;
     }
 
@@ -21,7 +25,6 @@ public class CustomBeanPostProcessor implements BeanPostProcessor {
         if(bean instanceof LifeCycleDemoBean){
             ((LifeCycleDemoBean) bean).afterInit();
         }
-
         return bean;
     }
 }
