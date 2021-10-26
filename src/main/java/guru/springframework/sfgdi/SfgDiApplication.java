b30a4940cb216e97dfd33e0e16291311fcce6b18
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-26 16:11:55 +0200

    using properties source presenting with a fakedatasource

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.services.PrototypeBean;
 import guru.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
@@ -94,6 +95,7 @@ public class SfgDiApplication {
 		System.out.println(bestPet);
 
 		System.out.println("---- Bean Scopes ----");
+
 		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
 		System.out.println(singletonBean1.getMyScope());
 		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
@@ -103,5 +105,11 @@ public class SfgDiApplication {
 		System.out.println(prototypeBean1.getMyScope());
 		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
 		System.out.println(prototypeBean2.getMyScope());
+
+		System.out.println("-- Fake DataSource --");
+		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUsername());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getJdbcUrl());
  	}
 }
